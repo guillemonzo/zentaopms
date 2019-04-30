567249cd9f7ec5e65ebdff86fbcc32458a135fe3
Guille <guillemonzo@gmail.com>
2019-04-30 13:21:18 +0200

    Added DB config based on environment variables.

---
+++ config/my.php
@@ -0,0 +1,17 @@
+<?php
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$config->installed       = false;
+$config->debug           = false;
+$config->requestType     = 'PATH_INFO';
+$config->timezone        = 'Europe/Madrid';
+$config->db->host        = $dbparts['host'];
+$config->db->port        = '3306';
+$config->db->name        = ltrim($dbparts['path'],'/');
+$config->db->user        = $dbparts['user'];
+$config->db->password    = $dbparts['pass'];
+$config->db->prefix      = 'zt_';
+$config->webRoot         = getWebRoot();
+$config->default->lang   = 'en';
